f1ec86264a7a8928d6700cfe7ca85995e7d044f1
inaralmeida <inara.ralmeida@gmail.com>
2023-10-14 20:05:47 -0300

    feat: data page

---
+++ src/core/data.ts
@@ -0,0 +1,52 @@
+import { IContetHistorysProps } from "./interfaces";
+
+export const contentHistorias: IContetHistorysProps[] = [
+  {
+    id: "um",
+    title: "história um",
+    titlePage: "historia-um",
+    interviewed: "Renan Marques",
+    photoInterviewed: "/historias/pessoaDois.jpg",
+    headline: "Noticia extremamente bombastica",
+    phraseImpact: "Crescer é aprender a ouvir",
+    nameAuthor: "Inara Almeida",
+    linkPodcast: "/historias/lud47min.ogg",
+    publicationDate: "14 de Outubro de 2023",
+    text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean elementum lacus ut mi maximus ullamcorper. Aliquam scelerisque tortor elementum, laoreet justo vel, imperdiet diam. Donec a ligula efficitur, mattis ante vel, egestas lacus. Praesent finibus sed leo non sodales. Quisque id urna efficitur, vehicula augue id, pellentesque odio. Donec at vulputate purus, at pretium sem. Cras eleifend non massa sed dignissim. Sed ultrices quis eros at varius. Sed sapien risus, scelerisque lobortis elit a, blandit ornare enim. Aenean volutpat pretium cursus. Cras sed vestibulum erat, sit amet rutrum nibh. In ut placerat sapien.
+    Cras vitae malesuada risus. Aenean egestas pulvinar arcu et fermentum. Ut hendrerit massa cursus commodo euismod. Quisque a nisl vel ex efficitur aliquet sed et tortor. Nam semper velit in velit molestie dictum. In hac habitasse platea dictumst. In placerat vulputate odio, vitae luctus velit cursus euismod. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+    sit amet rutrum nibh. In ut placerat sapien.
+    Cras vitae malesuada risus. Aenean egestas pulvinar arcu et fermentum. Ut hendrerit massa cursus commodo euismod. Quisque a nisl vel ex efficitur aliquet sed et tortor. Nam semper velit in velit molestie dictum. In hac habitasse platea dictumst. In placerat vulputate odio, vitae luctus veli
+    
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean elementum lacus ut mi maximus ullamcorper. Aliquam scelerisque tortor elementum, laoreet justo vel, imperdiet diam. Donec a ligula efficitur, mattis ante vel, egestas lacus. Praesent finibus sed leo non sodales. Quisque id urna efficitur, vehicula augue id, pellentesque odio. Donec at vulputate purus, at pretium sem. Cras eleifend non massa sed dignissim. Sed ultrices quis eros at varius. Sed sapien risus, scelerisque lobortis elit a, blandit ornare enim. Aenean volutpat pretium cursus. Cras sed vestibulum erat, sit amet rutrum nibh. In ut placerat sapien.
+Cras vitae malesuada risus. Aenean egestas pulvinar arcu et fermentum. Ut hendrerit massa cursus commodo euismod. Quisque a nisl vel ex efficitur aliquet sed et tortor. Nam semper velit in velit molestie dictum. In hac habitasse platea dictumst. In placerat vulputate odio, vitae luctus velit cursus euismod. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+sit amet rutrum nibh. In ut placerat sapien.
+Cras vitae malesuada risus. Aenean egestas pulvinar arcu et fermentum. Ut hendrerit massa cursus commodo euismod. Quisque a nisl vel ex efficitur aliquet sed et tortor. Nam semper velit in velit molestie dictum. In hac habitasse platea dictumst. In placerat vulputate odio, vitae luctus veliLorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean elementum lacus ut mi maximus ullamcorper. Aliquam scelerisque tortor elementum, laoreet justo vel, imperdiet diam. Donec a ligula efficitur, mattis ante vel, egestas lacus. Praesent finibus sed leo non sodales. Quisque id urna efficitur, vehicula augue id, pellentesque odio. Donec at vulputate purus, at pretium sem. Cras eleifend non massa sed dignissim. Sed ultrices quis eros at varius. Sed sapien risus, scelerisque lobortis elit a, blandit ornare enim. Aenean volutpat pretium cursus. Cras sed vestibulum erat, sit amet rutrum nibh. In ut placerat sapien.
+Cras vitae malesuada risus. Aenean egestas pulvinar arcu et fermentum. Ut hendrerit massa cursus commodo euismod. Quisque a nisl vel ex efficitur aliquet sed et tortor. Nam semper velit in velit molestie dictum. In hac habitasse platea dictumst. In placerat vulputate odio, vitae luctus velit cursus euismod. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+sit amet rutrum nibh. In ut placerat sapien.
+Cras vitae malesuada risus. Aenean egestas pulvinar arcu et fermentum. Ut hendrerit massa cursus commodo euismod. Quisque a nisl vel ex efficitur aliquet sed et tortor. Nam semper velit in velit molestie dictum. In hac habitasse platea dictumst. In placerat vulputate odio, vitae luctus veli`,
+  },
+  {
+    id: "dois",
+    title: "história dois",
+    titlePage: "historia-dois",
+    interviewed: "Rene Marques",
+    photoInterviewed: "/historias/pessoaUm.jpg",
+    headline: "Noticia extremamente bombastica",
+    phraseImpact: "Crescer é aprender a ouvir",
+    nameAuthor: "Inara Almeida",
+    linkPodcast: "/historias/musica1.ogg",
+    publicationDate: "14 de Outubro de 2023",
+    text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean elementum lacus ut mi maximus ullamcorper. Aliquam scelerisque tortor elementum, laoreet justo vel, imperdiet diam. Donec a ligula efficitur, mattis ante vel, egestas lacus. Praesent finibus sed leo non sodales. Quisque id urna efficitur, vehicula augue id, pellentesque odio. Donec at vulputate purus, at pretium sem. Cras eleifend non massa sed dignissim. Sed ultrices quis eros at varius. Sed sapien risus, scelerisque lobortis elit a, blandit ornare enim. Aenean volutpat pretium cursus. Cras sed vestibulum erat, sit amet rutrum nibh. In ut placerat sapien.
+    Cras vitae malesuada risus. Aenean egestas pulvinar arcu et fermentum. Ut hendrerit massa cursus commodo euismod. Quisque a nisl vel ex efficitur aliquet sed et tortor. Nam semper velit in velit molestie dictum. In hac habitasse platea dictumst. In placerat vulputate odio, vitae luctus velit cursus euismod. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+    sit amet rutrum nibh. In ut placerat sapien.
+    Cras vitae malesuada risus. Aenean egestas pulvinar arcu et fermentum. Ut hendrerit massa cursus commodo euismod. Quisque a nisl vel ex efficitur aliquet sed et tortor. Nam semper velit in velit molestie dictum. In hac habitasse platea dictumst. In placerat vulputate odio, vitae luctus veli
+    
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean elementum lacus ut mi maximus ullamcorper. Aliquam scelerisque tortor elementum, laoreet justo vel, imperdiet diam. Donec a ligula efficitur, mattis ante vel, egestas lacus. Praesent finibus sed leo non sodales. Quisque id urna efficitur, vehicula augue id, pellentesque odio. Donec at vulputate purus, at pretium sem. Cras eleifend non massa sed dignissim. Sed ultrices quis eros at varius. Sed sapien risus, scelerisque lobortis elit a, blandit ornare enim. Aenean volutpat pretium cursus. Cras sed vestibulum erat, sit amet rutrum nibh. In ut placerat sapien.
+Cras vitae malesuada risus. Aenean egestas pulvinar arcu et fermentum. Ut hendrerit massa cursus commodo euismod. Quisque a nisl vel ex efficitur aliquet sed et tortor. Nam semper velit in velit molestie dictum. In hac habitasse platea dictumst. In placerat vulputate odio, vitae luctus velit cursus euismod. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+sit amet rutrum nibh. In ut placerat sapien.
+Cras vitae malesuada risus. Aenean egestas pulvinar arcu et fermentum. Ut hendrerit massa cursus commodo euismod. Quisque a nisl vel ex efficitur aliquet sed et tortor. Nam semper velit in velit molestie dictum. In hac habitasse platea dictumst. In placerat vulputate odio, vitae luctus veliLorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean elementum lacus ut mi maximus ullamcorper. Aliquam scelerisque tortor elementum, laoreet justo vel, imperdiet diam. Donec a ligula efficitur, mattis ante vel, egestas lacus. Praesent finibus sed leo non sodales. Quisque id urna efficitur, vehicula augue id, pellentesque odio. Donec at vulputate purus, at pretium sem. Cras eleifend non massa sed dignissim. Sed ultrices quis eros at varius. Sed sapien risus, scelerisque lobortis elit a, blandit ornare enim. Aenean volutpat pretium cursus. Cras sed vestibulum erat, sit amet rutrum nibh. In ut placerat sapien.
+Cras vitae malesuada risus. Aenean egestas pulvinar arcu et fermentum. Ut hendrerit massa cursus commodo euismod. Quisque a nisl vel ex efficitur aliquet sed et tortor. Nam semper velit in velit molestie dictum. In hac habitasse platea dictumst. In placerat vulputate odio, vitae luctus velit cursus euismod. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+sit amet rutrum nibh. In ut placerat sapien.
+Cras vitae malesuada risus. Aenean egestas pulvinar arcu et fermentum. Ut hendrerit massa cursus commodo euismod. Quisque a nisl vel ex efficitur aliquet sed et tortor. Nam semper velit in velit molestie dictum. In hac habitasse platea dictumst. In placerat vulputate odio, vitae luctus veli`,
+  },
+];
