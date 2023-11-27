8582c2b108d90054baefc3ce2be7976b25d7dca3
inaralmeida <inara.ralmeida@gmail.com>
2023-11-27 18:51:54 -0300

    feat: home

---
+++ src/core/home.ts
@@ -0,0 +1,31 @@
+export const homeData = [
+  {
+    title:`HISTÓRIAS DE VIDA`,
+    preview:`Aqui você pode conferir o Podcast Relatos Paralimpicos, um espaço inspirador onde mergulhamos nas vidas de atletas que desafiaram todas as probabilidades. Cada episódio apresenta relatos de atletas paralímpicos, revelando suas jornadas de superação, desde os desafios enfrentados até as vitórias alcançadas.`,
+    by:`Lais Gonella`,
+    image:`/banners/banner_historiasdevida_pp.jpeg`,
+    to:`/historias/daniel-dias`,
+  },
+  {
+    title:`REPRESENTATIVIDADE FEMININA`,
+    preview:`Os Jogos Paralímpicos são um evento esportivo de prestígio global que celebra a excelência de atletas com deficiência. Apesar do progresso notável em termos de inclusão nos últimos anos, a representatividade feminina na competição ainda é uma questão que merece destaque.`,
+    by:`Rebeca Barreto da Silva`,
+    image:`/banners/busca_representatividade.jpeg`,
+    to:`/representatividade/historia-um`,
+  },
+  {
+    title:`BUSCA POR APOIO`,
+    preview:`A falta de apoio financeiro do paradesporto revela um cenário desafiador, no qual os atletas enfrentam obstáculos para obter o apoio e o reconhecimento que merecem. A baixa visibilidade, investimentos limitados e barreiras sociais ainda persistem, impedindo o desenvolvimento esportivo que era para ser tão valioso. `,
+    by:`Giovanni V. Panatto`,
+    image:`/banners/busca_Por_Apop_banner.jpeg`,
+    to:`http://localhost:5173/apoio/historia-um`,
+  },
+  {
+    title:`TECNOLOGIA`,
+    preview:`A tecnologia vem avançando em todos os seus meios e trazendo grandes evoluções e recursos para a vida. Nos atletas, isso não é diferente; mudanças consideráveis na vida e no desempenho esportivo mostram como a tecnologia veio para auxiliar tanto na parte profissional quanto na pessoal. Outra parte essencial no desempenho dos atletas é a fisioterapia e, claro, com o avanço da tecnologia, uma área acaba impulsionando a outra, visando sempre a melhoria de vida.`,
+    by:`Leandro Fazano Cardoso`,
+    image:`/banners/banner_tecnologia.jpg`,
+    to:`/tecnologia/historia-um`,
+  },
+ 
+]
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,121 @@
+import styled from "styled-components";
 import Banner from "../../components/Banner/Banner";
+import { homeData } from "../../core/home";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate()
   return (
     <div>
       <Banner
         title=""
         image="https://swimchannel.net/br/wp-content/uploads/2020/09/92a1fb48105b462e8ca2efde677ecee1.jpg"
       />
+
+      <StylesHome>
+        {homeData.map((item, id) => {
+          return (
+            <div key={id} className="card" onClick={() => navigate(item.to)}>
+              <div className="picture">
+                <img src={item.image} alt={`Capa da pagina ${item.title}`} />
+              </div>
+              <div className="contentCard">
+                <h3>{item.title}</h3>
+                <small>Por: {item.by}</small>
+                <hr />
+                <p>{item.preview}</p>
+
+                <footer>
+                <a href={item.to}>Leia mais</a>
+              </footer>
+              </div>
+              
+            </div>
+          );
+        })}
+      </StylesHome>
     </div>
   );
 };
 
 export default Home;
+
+const StylesHome = styled.main`
+  width: 100%;
+  max-width: 1200px;
+  padding: 24px;
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  margin: 0 auto;
+
+  > .card {
+    display: flex;
+    align-items: flex-start;
+    gap: 14px;
+    margin-bottom: 24px;
+    background-color: aliceblue;
+    border-radius: 0 8px 8px 0;
+    box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.25);
+    cursor: pointer;
+
+    transition: all 0.3s;
+    &:hover{
+      transform: scale(1.02);
+    }
+
+    .picture{
+      width: 350px;
+      height: 200px;
+      display: flex;
+      align-items: flex-start;
+      justify-content: center;
+      border-radius: 8px;
+    }
+
+
+    img {
+        width: 350px;
+        height: 200px;
+        object-fit: cover;
+      border-radius: 8px;
+
+      }
+
+    > .contentCard {
+      padding: 12px;
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      gap: 5px;
+      
+
+      > hr{
+        width: 100%;
+        height: 1px;
+      }
+
+      > footer{
+        width: 100%;
+        display: flex;
+        align-items: flex-end;
+        justify-content: flex-end;
+        align-self: flex-end;
+
+        > a{
+          cursor: pointer;
+          transition: all 0.3s;
+        }
+      }
+    }
+  }
+
+  @media screen and (max-width: 768px) {
+    > .card{
+      flex-direction: column;
+      > .picture{
+        width: 100%;
+      }
+    }
+  }
+`;
