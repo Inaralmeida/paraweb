549dbd4ef5f78e2a48fd002bfcef55189b4f3571
inaralmeida <inara.ralmeida@gmail.com>
2023-11-27 00:51:08 -0300

    feat: page sobre

--- src/pages/Sobre/Sobre.tsx
+++ src/pages/Sobre/Sobre.tsx
@@ -1,5 +1,34 @@
+import styled from "styled-components";
+import MiniBio from "../../components/MiniBio/MiniBio";
+import { miniBioData } from "../../core/MiniBio";
+
 const Sobre = () => {
-  return <div>Sobre</div>;
+  
+  return <StylesSobre>
+    {miniBioData.map((bio) => (
+      <MiniBio
+        name={bio.name}
+        age={bio.age}
+        photo={bio.photo}
+        text={bio.text}
+        email={bio.email}
+        alt={bio.alt}
+      />
+    ))}
+  </StylesSobre>;
+
 };
 
 export default Sobre;
+
+const StylesSobre = styled.main`
+padding: 24px;
+margin: 0 auto;
+ width: 100%;
+  max-width: 900px;
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  column-gap: 20px;
+  `
