cd65110931099d66f706eb5b7d3ef1e62f5067b9
inaralmeida <inara.ralmeida@gmail.com>
2023-11-27 21:45:46 -0300

    fix: bug player in iphone

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -3,7 +3,7 @@ import ReactAudioPlayer from "react-audio-player";
 const Player = ({ url }: { url: string }) => {
   return (
     <div>
-      <ReactAudioPlayer controls src={url} />
+      <ReactAudioPlayer controls src={url} muted/>
     </div>
   );
 };
